0509a8b5c3a07801ccfebc94b62314988fe72fb1
Daniel Abadi <48056794+danielabadi@users.noreply.github.com>
2023-04-29 08:20:32 -0300

    Criação da tabela de imunizantes

---
+++ frontend/src/assets/data/InfoVaccines.js
@@ -0,0 +1,28 @@
+const InfoVaccines = [
+    { vacina: "BCG", doenca: "Formas graves de tuberculose, meníngea e miliar", esquema: "Dose única", idade: "Ao nascer", reforco: "Sem reforço" },
+    { vacina: "DTP", doenca: "Difteria, Tétano e Coqueluche", esquema: "Considerar doses anteriores com penta e DTP", idade: "15 meses", reforco: "Um reforço com a vacina DTP REF (4 e 6 anos)" },
+    { vacina: "dTpa gestante", doenca: "Difteria, Tétano e Coqueluche", esquema: "Uma dose para gestantes a partir da 20ª semana de gravidez", idade: "Sem idade recomendada", reforco: "Uma dose a cada gestação" },
+    { vacina: "DTP REF (4 e 6 anos)", doenca: "Difteria, Tétano e Coqueluche", esquema: "Considerar doses anteriores com penta e DTP", idade: "4 a 6 anos de idade", reforco: "Sem reforço" },
+    { vacina: "Dupla adulto e tríplice acelular gestante", doenca: "Difteria, Tétano e Coqueluche", esquema: "Uma dose para gestantes a partir da 20ª semana de gravidez", idade: "Sem idade recomendada", reforco: "Uma dose a cada gestação" },
+    { vacina: "Febre Amarela", doenca: "Febre Amarela", esquema: "Dose única, para pessoas que nunca foram vacinadas ou sem comprovante de vacinação", idade: "Para pessoas até 59 anos de idade não vacinadas", reforco: "Reforço, caso a pessoa tenha recebido uma dose da vacina antes de completar 5 anos de idade" },
+    { vacina: "Hepatite A", doenca: "Hepatite A", esquema: "Uma dose", idade: "15 meses", reforco: "Sem reforço" },
+    { vacina: "Hepatite B", doenca: "Hepatite B", esquema: "3 doses (Iniciar ou completar o esquema, de acordo com situação vacinal)", idade: "Sem idade recomendada", reforco: "Sem reforço" },
+    { vacina: "Hepatite B  em crianças até 30 dias", doenca: "Hepatite B", esquema: "Dose ao nascer", idade: "Ao nascer", reforco: "Reforço com a vacina Hepatite B" },
+    { vacina: "Meningococo C", doenca: "Meningite meningocócica sorogrupo C", esquema: "Duas doses", idade: "1ª dose: 3 meses, 2ª dose: 5 meses", reforco: "Reforço com a vacina Meningococo C (1º ref)" },
+    { vacina: "Meningococo C (1º ref)", doenca: "Meningite meningocócica sorogrupo C", esquema: "Uma dose", idade: "1 ano", reforco: "Sem reforço" },
+    { vacina: "Penta", doenca: "Difteria, Tétano, Coqueluche, Haemophilus influenzae B e Hepatite B", esquema: "Três doses", idade: "1ª dose: 2 meses, 2ª dose: 4 meses, 3ª dose: 6 meses", reforco: "Dois reforços com a vacina DTP" },
+    { vacina: "Pneumocócica", doenca: "Pneumonias, Meningites, Otites, Sinusites pelos sorotipos que compõem a vacina", esquema: "Duas doses", idade: "1ª dose: 2 meses, 2ª dose: 4 meses", reforco: "Reforço com a vacina Pneumocócica(1º ref)" },
+    { vacina: "Pneumocócica(1º ref)", doenca: "Pneumonias, Meningites, Otites, Sinusites pelos sorotipos que compõem a vacina", esquema: "Uma dose", idade: "1 ano", reforco: "Sem reforço" },
+    { vacina: "Poliomielite", doenca: "Poliomielite", esquema: "Três doses", idade: "1ª dose: 2 meses, 2ª dose: 4 meses, 3ª dose: 6 meses", reforco: "Reforço com a vacina Poliomielite(1º ref) e Poliomielite 4 anos" },
+    { vacina: "Poliomielite 4 anos", doenca: "Poliomielite", esquema: "Uma dose", idade: "4 anos", reforco: "Sem reforço" },
+    { vacina: "Poliomielite(1º ref)", doenca: "Poliomielite", esquema: "Uma dose", idade: "15 meses", reforco: "Reforço com a vacina Poliomielite 4 anos" },
+    { vacina: "Rotavírus Humano", doenca: "Diarreia por Rotavírus", esquema: "Duas doses", idade: "1ª dose: 2 meses, 2ª dose: 4 meses", reforco: "Sem reforço" },
+    { vacina: "Tetra Viral(SRC+VZ)", doenca: "Sarampo, Caxumba, Rubéola e Varicela", esquema: "Duas doses (segunda dose da SCR e primeira de varicela)", idade: "15 meses", reforco: "Sem reforço" },
+    { vacina: "Tetravalente (DTP/Hib) (TETRA)", doenca: "Difteria, Tétano, Coqueluche e Haemophilus", esquema: "Três doses", idade: "1ª dose: 2 meses, 2ª dose: 4 meses, 3ª dose: 6 meses", reforco: "Dois reforços com a vacina DTP" },
+    { vacina: "Tríplice Bacteriana(DTP)(1º ref)", doenca: "Difteria, Tétano e Coqueluche", esquema: "Considerar doses anteriores com penta e DTP", idade: "15 meses", reforco: "Um reforço com a vacina DTP REF (4 e 6 anos)" },
+    { vacina: "Tríplice Viral  D1", doenca: "Sarampo, Caxumba e Rubéola", esquema: "Duas doses (primeira dose da SCR e segunda dose com SCRV)", idade: "1 ano", reforco: "Reforço com a vacina Tetra Viral(SRC+VZ) ou Tríplice Viral D2 + Varicela" },
+    { vacina: "Tríplice Viral  D2", doenca: "Sarampo, Caxumba e Rubéola", esquema: "Duas doses (primeira dose da SCR)", idade: "15 meses", reforco: "Sem reforço" },
+    { vacina: "Varicela", doenca: "Varicela", esquema: "Uma dose (corresponde à segunda dose da varicela)", idade: "4 anos", reforco: "Sem reforço" }
+]
+
+export default InfoVaccines;
---
+++ frontend/src/components/tableVaccines/TableVaccines.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./TableVaccines.css";
+import InfoVaccines from "../../assets/data/InfoVaccines";
+
+function TableVaccines() {
+    return (
+        <table className="vacinas">
+            <thead>
+                <tr>
+                    <th>Vacina</th>
+                    <th>Proteção Contra</th>
+                    <th>Esquema Básico</th>
+                    <th>Reforço</th>
+                    <th>Idade recomendada</th>
+                </tr>
+            </thead>
+            <tbody>
+                {InfoVaccines.map(value => (
+                    <tr key={value.vacina}>
+                        <td className="vacinas--nome">{value.vacina}</td>
+                        <td>{value.doenca}</td>
+                        <td>{value.esquema}</td>
+                        <td>{value.reforco}</td>
+                        <td>{value.idade}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TableVaccines;
